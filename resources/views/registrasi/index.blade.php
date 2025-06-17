b4004c452c77c920446f0bee4343171e3e7cd981
rapipn02 <jordisimilikiti@gmail.com>
2025-06-17 22:44:59 +0700

    change logo register

--- resources/views/registrasi/index.blade.php
+++ resources/views/registrasi/index.blade.php
@@ -32,7 +32,7 @@
                     <div
                         class="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2">
                         <div class="login-brand">
-                                class="shadow-light rounded-circle">
+    <img src="{{ asset('assets/img/Logo-Hima.png') }}" alt="Logo Hima" width="100" class="shadow-light rounded-circle">
                         </div>
 
                         <div class="card card-primary">
